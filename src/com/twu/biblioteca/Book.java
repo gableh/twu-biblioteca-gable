5c3aa8eb2abb94262a2e3387c42d5b1cb53ebacd
Gable Heng <gableh@thoughtworks.com>
2018-09-09 00:31:03 +0800

    add book comparator

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -3,17 +3,36 @@ package com.twu.biblioteca;
 public class Book {
     private String author_name;
     private int year_published;
+    private String title;
 
-    public Book(String author_name, int year_published) {
+    Book(String title, String author_name, int year_published) {
+        this.title = title;
         this.author_name = author_name;
         this.year_published = year_published;
     }
 
-    public int getYearPublished() {
+    int getYearPublished() {
         return year_published;
     }
 
-    public String getAuthorName() {
+    String getAuthorName() {
         return author_name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if(this.getClass() != obj.getClass()) {
+            return false;
+        }
+        final Book other = (Book) obj;
+
+        boolean hasSameAuthor = this.author_name.equals(other.author_name);
+        boolean hasSameTitle = this.title.equals(other.title);
+        boolean hasSameYearPublished = this.year_published == other.year_published;
+
+        return hasSameAuthor && hasSameTitle && hasSameYearPublished;
     }
 }
--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -1,20 +1,31 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class BookTest {
 
     @Test
     public void itShouldReturnAuthorName() {
-        Book book = new Book("author", 1993);
+        Book book = new Book("book1","author", 1993);
         assertEquals("author", book.getAuthorName());
     }
 
     @Test
     public void itShouldReturnYearPublished() {
-        Book book = new Book("author", 1993);
+        Book book = new Book("book2", "author", 1993);
         assertEquals(1993, book.getYearPublished());
+    }
+
+    @Test
+    public void itShouldBeAbleToCompareWithEachOther() {
+        Book book1 = new Book("book2", "author", 1993);
+        Book book2 = new Book("book2", "author", 1993);
+        Book book3 = new Book("book3", "author", 1993);
+        assertEquals(book1, book2);
+        assertNotEquals(book2, book3);
     }
 
 }
